51201ed25a9b54bdc7be0c44e501f13004252558
김선규 <seongyu1066@naver.com>
2023-03-16 17:04:56 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 236 ms, Memory: 18336 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int k = 1, q = 1, l = 2, b = 2, n = 2, p = 8;
+
+	
+		k = k - sc.nextInt();
+		q = q - sc.nextInt();
+		l = l - sc.nextInt();
+		b = b - sc.nextInt();
+		n = n - sc.nextInt();
+		p = p - sc.nextInt();
+		
+		System.out.print(k + " ");
+		System.out.print(q + " ");
+		System.out.print(l + " ");
+		System.out.print(b + " ");
+		System.out.print(n + " ");
+		System.out.print(p + " ");
+		
+	}
+
+}
